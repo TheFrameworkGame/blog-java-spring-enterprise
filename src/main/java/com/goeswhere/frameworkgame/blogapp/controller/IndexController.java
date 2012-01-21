453708d55798cb4380a12dad83cd0c8da574c9e1
Chris West (Faux) <git@goeswhere.com>
2012-01-21 23:25:12 +0000

    HOLY SHIT IT RAN SQL

--- src/main/java/com/goeswhere/frameworkgame/blogapp/controller/IndexController.java
+++ src/main/java/com/goeswhere/frameworkgame/blogapp/controller/IndexController.java
@@ -1,18 +1,23 @@
 package com.goeswhere.frameworkgame.blogapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.goeswhere.frameworkgame.blogapp.domain.Post;
-import com.google.common.collect.Lists;
+import com.goeswhere.frameworkgame.blogapp.dao.PostDao;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+	@Autowired
+	PostDao posts;
+
+	@Transactional(readOnly = true)
 	@RequestMapping
 	public String index(Model m) {
-		m.addAttribute("posts", Lists.newArrayList(new Post("Ponies!", "All about ponies")));
+		m.addAttribute("posts", posts.all());
 		return "index";
 	}
 }
--- src/main/java/com/goeswhere/frameworkgame/blogapp/dao/PostDao.java
+++ src/main/java/com/goeswhere/frameworkgame/blogapp/dao/PostDao.java
@@ -1,5 +1,26 @@
 package com.goeswhere.frameworkgame.blogapp.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.goeswhere.frameworkgame.blogapp.domain.Post;
+
+@Repository
 public class PostDao {
 
+	@PersistenceContext
+	EntityManager em;
+
+	@Transactional(readOnly = true)
+	public List<Post> all() {
+		final CriteriaQuery<Post> cq = em.getCriteriaBuilder().createQuery(Post.class);
+		cq.from(Post.class);
+		return em.createQuery(cq).getResultList();
+	}
 }
